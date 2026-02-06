0eb5f5a4d54f897edbe854edbedce2f07870dbad
sshehrozali <sshehroza13@gmail.com>
2026-02-06 20:27:04 +0500

    added log

--- src/main/java/com/platform/api/ApiApplication.java
+++ src/main/java/com/platform/api/ApiApplication.java
@@ -1,13 +1,18 @@
 package com.platform.api;
 
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiApplication {
 
+	private final static Logger logger = LoggerFactory.getLogger(ApiApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("Pulumi initialised");
 		SpringApplication.run(ApiApplication.class, args);
 	}
 }
